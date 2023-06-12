407d50b9e81e454951f828e24423f03d3d0b3d41
Michael <michaellngriffin@gmail.com>
2023-06-12 15:39:17 -0400

    selectionSort added, svg icons now included.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from "react"
 import {useState} from "react"
+import svgIcons from "./icons/svg_icons"
 
 function App() {
   let heightval = 100;
-
+  console.log(svgIcons);
   //Still to do:
     //Merge, Heap, Selection, QuickSort.
 
-  //For animations: could have a gradient color for bars (maybe a little too fancy)
-  //Could have value below bars if there are few enough
+
   //Need a finish/clean up step for animations.
+  //Excessive remaking of animations array during animate?
+    //Currently, animations array are regenerated as animations play. We really only want a new animation list
+    //when a new array is generated. Not sure whether useEffect/useMemo makes sense here, but if so could set
+    //dependency array equal to the randomly generated array. For now, as long as the animation only runs once
+    //it's fine.
 
   //Styling wise, couple of options:
+
+  //For animations:
+    //gradient color for bars? (maybe a little too fancy)
+    //value below bars (only appear if barnum < constant)
   //For side menu
     //could bring selected to the top
     //OR could bold, increase text-size. (slightly prefer this)
-    //If I keep buttons, will have to style away most of the 'button-ness'
-    //could change to <a> tags or just plain divs, tie an onclick function there
-  //For Footer
-    //Could add a top header (controls, like demo I saw)
-    //add icons for play/pause/speed.
+    //For above, might make sense to have a separate state for sort choice options (re-make list of buttons?)
+    //For footer, need to fix spacing (should have an overall container with space-between to spread out the stuff?)
+  //For bottom controls
+    //Could add a top header (controls, like in demo)
+    
 
 
   //Work out sorting visualizer for bubbleSort
@@ -69,9 +78,37 @@ function App() {
     }
     return anims;
   }
-  let animArr = bubbleSort(barsMain.vals);
+
+
+  function selectionSort(arrbase){
+    let arr = arrbase.slice(0);
+    let anims = [];
+    let cmin;
+
+    for (let n = 0; n < arr.length; n++){
+        cmin = n;
+        for (let o = n+1; o < arr.length; o++){
+            anims.push({first: n, second: o, type:'compare'}) 
+            if (arr[o] < arr[cmin]) cmin = o;
+        }
+        if (cmin !== n){
+            anims.push({first: n, second: cmin, type:'preswap'})
+            anims.push({first: n, second: cmin, type:'swap'})
+            swap(arr, n, cmin);
+        } 
+    }
+    return anims;
+  }
+
+  
   console.log('arr vals is', barsMain.vals);
-  console.log('animation list is', animArr);
+  let animArr = selectionSort(barsMain.vals);
+  console.log('selectSort animation list is', animArr);
+
+  let animArr1 = bubbleSort(barsMain.vals);
+  console.log('bubbleSort animation list is', animArr1);
+
+
 
   let animDemo = [
     {first: 0, second: 0, type:'nochange'},
@@ -101,12 +138,10 @@ function App() {
       //Currently, this changes colors, but doesn't change them back?
       //might have to grab prevAnim so we can switch stuff back.
       //Also need to add a check for anim Type (only switch colors if type=compare)
-      if (i == 1) console.log('made it to for loop', i);
-      if (i == 2) console.log('made it to for loop', i);
       if (cAnim.type === "nochange") continue; //Maybe unneeded, since we start at 1.
 
 
-      if (i == 2) console.log('made it past continue');
+      //if (i == 2) console.log('made it past continue');
       //Make the callback function for setTimeout
       let updateBars = () => {
         //Updates state of bars! Does three things:
@@ -142,7 +177,7 @@ function App() {
       setTimeout(updateBars, delay);
     }
 
-    //A note: if implementing a pause, can check:
+    //Note: if implementing a pause, can check:
     //https://stackoverflow.com/questions/8860188/javascript-clear-all-timeouts
     //This allows you to iteratively clear all timeout functions you set.
   }
@@ -182,41 +217,55 @@ function App() {
         <footer className="side-bottom">
           <div className="side-bottom github-logo">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 
-              13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 
-              2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 
-              4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 
-              3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038
-              0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052
-              13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324
-              24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364
-              19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#fff"/>
+              <path fillRule="evenodd" clipRule="evenodd" d={svgIcons.github} fill="#fff"/>
             </svg>
           </div>
           <div className="side-bottom github-text">
             <p>Michael Griffin's</p>
             <p>Github</p>
           </div>
-          <div className="side-bottom min-button">Min B</div>
+          <button className="side-bottom min-button">
+            <svg className="play-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path fillRule="evenodd" clipRule="evenodd" d={svgIcons.minimize} fill="#fff"/>
+            </svg>
+          </button>
         </footer>
         
       </aside>
 
       <main>
         <section className="array-container">
-          {barsJSX}        
+          {barsJSX /*what does 90% of the work*/}        
         </section>
 
         <footer className="controls">
           <div className="controls-buttons">
-            <button>Play</button>
-            <button>Pause</button>
+            <button>
+              <svg className="play-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path fillRule="evenodd" clipRule="evenodd" d={svgIcons.play} fill="#000"/>
+              </svg>
+             </button>
+            <button>
+              <svg className="pause-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path fillRule="evenodd" clipRule="evenodd" d={svgIcons.pause} fill="#000"/>
+              </svg>
+            </button>
           </div>
-          <div className="controls-sliders">
+          <div className="controls-speed">
+            <svg className="speed-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path fillRule="evenodd" clipRule="evenodd" d={svgIcons.speed} fill="#000"/>
+            </svg>
             <label htmlFor="speed">Sorting Speed</label>
             <input type="range" name="speed" min="10" max="100"></input>
+          </div>
+          <div className="controls-nbars">
             <input type="number" name="size" placeholder="# of bars" min="10" max="100"></input>
           </div>
+          <button className="controls-newArray">
+            <svg className="refresh-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path fillRule="evenodd" clipRule="evenodd" d={svgIcons.refresh} fill="#000"/>
+            </svg>
+          </button>
         </footer>
       </main>
     </div>
@@ -291,17 +340,17 @@ export default App;
 </svg>
 
 
-//Github Logo
-<svg xmlns="http://www.w3.org/2000/svg" width="98" height="96">
-  <path fill-rule="evenodd" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 
-  13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 
-  2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 
-  4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 
-  3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038
-  0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052
-  13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324
-  24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364
-  19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#fff"/>
-</svg>
+//WORKING github logo
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+    <path fillRule="evenodd" clipRule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 
+    13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 
+    2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 
+    4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 
+    3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038
+    0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052
+    13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324
+    24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364
+    19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#fff"/>
+  </svg>
 */
 
